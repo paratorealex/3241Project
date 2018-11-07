dfe804e26220b4a22da2b50c3cc6f9d90c658165
Alex Paratore <32722024+paratorealex@users.noreply.github.com>
2018-11-07 13:26:31 -0500

    Create populate.sql

---
+++ populate.sql
@@ -0,0 +1,27 @@
+INSERT INTO CUSTOMERS
+VALUES ('Joe', 'Smith', 'joesmith@email.com',1, 1111111111,'credit card','2018-08-15')
+
+INSERT INTO MAKERS
+VALUES (1,1111111111,'red')
+
+INSERT INTO DISTRIBUTOR
+VALUES (2, 'bb',1111111112,'the moon')
+
+INSERT INTO PRODUCT
+VALUES(74570014002, 'Chocolate', 'pint', 8, 'Haagen-Dazs', 1, 4, NULL, 4.57, 'Vegetarian', 5.49, 'CheckoutCooler', 'left', 'top', 'Frozen', 1, 2)
+
+
+INSERT INTO MANAGERS
+VALUES (1,'Joe','Smith','joesmith@email.com',2111111111,'222-23-2222','1990-01-01','2010-01-01')
+
+
+INSERT INTO STOREASSOCIATES
+VALUES (1,'joe','Dsmith','joe@email.com', 1111111111,'333-33-3333','1990-01-01','2010-01-01', 2111111111 )
+
+insert into coupon
+values (2749274638 , 3.76, 1);
+
+insert into orders
+values (38374, '01:13:33', 1, 1111091127);
+    
+ 
