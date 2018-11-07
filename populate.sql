3cd380db4ab32174cbbff69aa66e081b81d64456
Alex Paratore <32722024+paratorealex@users.noreply.github.com>
2018-11-07 14:42:01 -0500

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -5,10 +5,12 @@ INSERT INTO MAKER
 VALUES (1,1111111111,'red')
 
 INSERT INTO DISTRIBUTOR
-VALUES (2, 'bb',1111111112,'the moon')
+VALUES (1, 'A&B Distributors',1111111111,'the moon'),
+(2, 'FrozenGourmet, Inc.', 1111111112,'Columbus'),
+(3, 'Jones Truck Farms', 1111111113,'Cleveland')
 
 INSERT INTO PRODUCT
-VALUES(74570014002, 'Chocolate', 'pint', 8, 'Haagen-Dazs', 1, 4, NULL,2, 4.57, 'Vegetarian', 5.49, 'CheckoutCooler', 'left', 'top', 'Frozen', 1)
+VALUES(74570014002, 'Chocolate', 'pint', 8, 'Haagen-Dazs', 1, 4, NULL, 2, 4.57, 'Vegetarian', 5.49, 'CheckoutCooler', 'left', 'top', 'Frozen', 1)
 
 
 INSERT INTO MANAGERS
