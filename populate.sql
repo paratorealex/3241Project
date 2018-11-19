20794038130087f7e9838e094ebeeea84b35bced
Alex Paratore <32722024+paratorealex@users.noreply.github.com>
2018-11-18 21:34:59 -0500

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,29 +1,68 @@
+insert into TAGS
+values ('Frozen'),
+('Dessert'),
+('Produce'),
+('Energy'),
+('Powers'),
+('Made by hand')
+
+insert into BRANDS
+values ('Haagen-Dazs')
+
+
+insert into CERTIFICATIONS
+values ('Vegetarian')
+
+
+insert into DISTRIBUTORS
+values (1,'A&B Distributors','1001001000','123 main street')
+
+
+insert into MAKERS
+VALUES (1,'Nestle','595-454-3059', 'Haagen-Dazs')
+
+
 INSERT INTO CUSTOMERS
-VALUES ('Joe', 'Smith', 'joesmith@email.com',1, 1111111111,'credit card','2018-08-15')
-
-INSERT INTO MAKER
-VALUES (1,1111111111,'red')
-
-INSERT INTO DISTRIBUTOR
-VALUES (1, 'A&B Distributors',1111111111,'the moon'),
-(2, 'FrozenGourmet, Inc.', 1111111112,'Columbus'),
-(3, 'Jones Truck Farms', 1111111113,'Cleveland')
-
-INSERT INTO PRODUCT
-VALUES(74570014002, 'Chocolate', 'pint', 8, 'Haagen-Dazs', 1, 4, NULL, 2, 4.57, 'Vegetarian', 5.49, 'CheckoutCooler', 'left', 'top', 'Frozen', 1)
-
+VALUES (1,'Joe', 'Smith', 'joesmith@email.com', '100-000-0000','4321323454349999')
 
 INSERT INTO MANAGERS
-VALUES (1,'Joe','Smith','joesmith@email.com',2111111111,'222-23-2222','1990-01-01','2010-01-01')
+VALUES (1,'John','Smith','johnsmith@email.com','211-111-1111','1990-01-01','222-23-2222','2010-01-01')
+
+insert into LOCATIONS
+values ('CheckoutCooler')
+
+INSERT INTO PRODUCTS
+VALUES('74570014002', 'Chocolate', 'pint', 1, 5.49, 8)
+
+insert into ORDERS
+values ('38374', '01:13:33', 1);
+
+INSERT INTO TAGGED_PRODUCTS
+VALUES('74570014002','Frozen')
+
+INSERT INTO CERT_PRODUCTS
+VALUES('74570014002','Vegetarian')
+
+INSERT INTO DIST_PRODUCTS
+VALUES(1,'74570014002', 4.57)
+
+INSERT INTO EMPLOYEES
+VALUES (1,'Nick','Brown','NickBrown@email.com','211-119-1111','1990-02-01','222-23-2222','2011-01-03', 1)
+
+insert into LOCATION_PRODUCTS
+values ('74570014002','CheckoutCooler','left','top',4,null)
+
+insert into COUPONS
+values ('2749274638','74570014002', 2.50);
+
+insert into COUPONS_USED
+values ('2749274638', 1);
+
+insert into ORDERED_PRODUCTS
+values ('38374','74570014002', 2)
 
 
-INSERT INTO STOREASSOCIATES
-VALUES (1,'joe','Dsmith','joe@email.com', 1111111111,'333-33-3333','1990-01-01','2010-01-01', 2111111111 )
 
-insert into coupon
-values (2749274638 , 3.76, 1);
 
-insert into orders
-values (38374, '01:13:33', 1, 1111091127);
     
  
