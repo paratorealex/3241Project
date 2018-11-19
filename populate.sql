20b04013c542fef46b4da3902b62bf37b012d2a2
Alex Paratore <32722024+paratorealex@users.noreply.github.com>
2018-11-19 13:48:48 -0500

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -15,27 +15,27 @@ values ('Vegetarian')
 
 
 insert into DISTRIBUTORS
-values (1,'A&B Distributors','1001001000','123 main street')
+values ('1','A&B Distributors','1001001000','123 main street')
 
 
 insert into MAKERS
-VALUES (1,'Nestle','595-454-3059', 'Haagen-Dazs')
+VALUES ('1','Nestle','595-454-3059', 'Haagen-Dazs')
 
 
 INSERT INTO CUSTOMERS
-VALUES (1,'Joe', 'Smith', 'joesmith@email.com', '100-000-0000','4321323454349999')
+VALUES ('1','Joe', 'Smith', 'joesmith@email.com', '100-000-0000','4321323454349999')
 
 INSERT INTO MANAGERS
-VALUES (1,'John','Smith','johnsmith@email.com','211-111-1111','1990-01-01','222-23-2222','2010-01-01')
+VALUES ('1','John','Smith','johnsmith@email.com','211-111-1111','1990-01-01','222-23-2222','2010-01-01')
 
 insert into LOCATIONS
 values ('CheckoutCooler')
 
 INSERT INTO PRODUCTS
-VALUES('74570014002', 'Chocolate', 'pint', 1, 5.49, 8)
+VALUES('74570014002', 'Chocolate', 'pint', '1', 5.49, 8)
 
 insert into ORDERS
-values ('38374', '01:13:33', 1);
+values ('38374', '01:13:33', '1');
 
 INSERT INTO TAGGED_PRODUCTS
 VALUES('74570014002','Frozen')
@@ -44,24 +44,25 @@ INSERT INTO CERT_PRODUCTS
 VALUES('74570014002','Vegetarian')
 
 INSERT INTO DIST_PRODUCTS
-VALUES(1,'74570014002', 4.57)
+VALUES('1','74570014002', 4.57)
 
 INSERT INTO EMPLOYEES
-VALUES (1,'Nick','Brown','NickBrown@email.com','211-119-1111','1990-02-01','222-23-2222','2011-01-03', 1)
+VALUES ('1','Nick','Brown','NickBrown@email.com','211-119-1111','1990-02-01','222-23-2222','2011-01-03', '1')
 
 insert into LOCATION_PRODUCTS
 values ('74570014002','CheckoutCooler','left','top',4,null)
 
 insert into COUPONS
-values ('2749274638','74570014002', 2.50);
+values ('2749274638', 2.50);
 
 insert into COUPONS_USED
-values ('2749274638', 1);
+values ('2749274638', '74570014002','38374');
 
 insert into ORDERED_PRODUCTS
 values ('38374','74570014002', 2)
 
-
+insert into IN_PERSON
+values ('1','38374') 
 
 
     
