d50d9fe78443869cd8f7f80ff29168e995c94e77
johnmcgue7 <44849525+johnmcgue7@users.noreply.github.com>
2018-11-20 13:11:37 -0500

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -23,7 +23,26 @@ VALUES ('1','Nestle','595-454-3059', 'Haagen-Dazs')
 
 
 INSERT INTO CUSTOMERS
-VALUES ('1','Joe', 'Smith', 'joesmith@email.com', '100-000-0000','4321323454349999')
+VALUES ('1','Joe', 'Smith', 'joesmith@email.com', '100-000-0000','4321323454349999'), 
+('2','John', 'Brown', 'johnbrown@email.com', '200-000-0000','4321323454349998'), 
+('3','Brian', 'White', 'brianwhite@email.com', '300-000-0000','4321323454349997'), 
+('4','Katie', 'Blue', 'katieblue@email.com', '400-000-0000','4321323454349996'), 
+('5','Miles', 'Black', 'milesblack@email.com', '500-000-0000','4321323454349995'), 
+('6','Brooke', 'Green', 'brookegreen@email.com', '600-000-0000','4321323454349994'), 
+('7','Megan', 'Brown', 'meganbrown@email.com', '700-000-0000','4321323454349993'), 
+('8','Jake', 'Red', 'jakered@email.com', '800-000-0000','4321323454349992'), 
+('9','Andrew', 'Yellow', 'andrewyellow@email.com', '90-000-0000','4321323454349991'), 
+('10','Sophie', 'Purple', 'sophiepurple@email.com', '110-000-0000','4321323454349990'),
+('11','Maria', 'Grey', 'mariagrey@email.com', '120-000-0000','4321323454349989'), 
+('12','Jason', 'Brown', 'jasonbrown@email.com', '130-000-0000','4321323454349988'), 
+('13','Abby', 'Low', 'abbylow@email.com', '140-000-0000','4321323454349987'), 
+('14','Anna', 'High', 'annahigh@email.com', '150-000-0000','4321323454349986'), 
+('15','Hannah', 'Black', 'lilyblack@email.com', '160-000-0000','4321323454349985'), 
+('16','Emily', 'Smith', 'emilysmith@email.com', '170-000-0000','4321323454349984'), 
+('17','Jack', 'Reed', 'jackreed@email.com', '180-000-0000','4321323454349983'), 
+('18','Danielle', 'Blue', 'danielleblue@email.com', '190-000-0000','4321323454349982'), 
+('19','Jason', 'High', 'jasonhigh@email.com', '200-000-0000','4321323454349981'), 
+('20','Jim', 'Blake', 'jimblake@email.com', '210-000-0000','4321323454349980')
 
 INSERT INTO MANAGERS
 VALUES ('1','John','Smith','johnsmith@email.com','211-111-1111','1990-01-01','222-23-2222','2010-01-01')
